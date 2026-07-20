bc6be436ab844af3768fe3d6593a5ea74cf7fbaf
LZY2333 <1114396573@qq.com>
2026-07-20 10:21:13 +0800

    chore: 清理旧项目并补充智能体配置

--- test-vue32/src/pages/imot4/billMapping/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import Antd from 'ant-design-vue';
-import App from './views/billMapping.vue'
-import 'ant-design-vue/dist/antd.less';
-
-const app = createApp(App);
-
-app.use(Antd).mount('#app');
